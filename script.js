5c23f7ab45c43ba729adbea5f91e6b3513d630d5
Artur Santana Pereira <112443860+artursantana@users.noreply.github.com>
2022-11-06 19:44:29 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let nav = document.getElementById('nav')
+
+
+function ver(){
+
+    if(nav.classList.contains('open')){
+        nav.classList.remove('open')
+    }else{
+        nav.classList.add('open')
+    }
+
+}
+
